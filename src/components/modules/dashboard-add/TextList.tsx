2cc535ded1e72d4276aadcf0d27e6ea8a5303d27
Amir Basirati <amirbasirati13@gmail.com>
2023-10-19 17:04:10 +0330

    text list logic in react hook form improved

--- src/components/modules/dashboard-add/TextList.tsx
+++ src/components/modules/dashboard-add/TextList.tsx
@@ -1,12 +1,12 @@
-import { Inputs } from "@/templates/AddProfilePage";
 import { UseFieldArrayAppend, UseFieldArrayRemove } from "react-hook-form";
 import { AiOutlineDelete } from "react-icons/ai";
 import { MdOutlineLibraryAdd } from "react-icons/md";
+import { Profile } from "src/types/Profile";
 
 interface Props {
   fields: Record<"id", string>[];
   remove: UseFieldArrayRemove;
-  append: UseFieldArrayAppend<Inputs, never>;
+  append: UseFieldArrayAppend<Profile, never>;
   register: any;
   name: string;
   pName: string;
@@ -28,8 +28,8 @@ const TextList: React.FC<Props> = ({
           <div className="flex my-4 mx-0" key={index}>
             <input
               className=" w-80 border border-meBlue border-dashed text-gray-600 rounded-md p-3 text-base h-8 ml-3 focus:border-solid focus:outline-none"
-              key={index}
-              {...register(`${name}.${index}`)}
+              key={item.id}
+              {...register(`${name}.${index}` as const)}
             />
             <button
               className=" m-0 text-[#db0505] bg-white border border-[#db0505] border-solid flex items-center leading-3 p-1 rounded"
