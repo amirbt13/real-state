4dda1c760584e3fbe194ab90a16306dca6beff1b
Amir Basirati <amirbasirati13@gmail.com>
2023-10-19 17:02:07 +0330

    Profile type added

---
+++ src/types/Profile.ts
@@ -0,0 +1,12 @@
+export type Profile = {
+  title: string;
+  description: string;
+  location: string;
+  phone: string;
+  price: string;
+  realState: string;
+  construction: Date;
+  category: string;
+  rules: string[];
+  amenities: string[];
+};
