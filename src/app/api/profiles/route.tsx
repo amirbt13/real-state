09cd30e656bc7e3e41931acaede7529d980195b9
Amir Basirati <amirbasirati13@gmail.com>
2023-10-23 16:02:13 +0330

    profiles api updated, now it handles query requests

--- src/app/api/profiles/route.tsx
+++ src/app/api/profiles/route.tsx
@@ -1,12 +1,21 @@
 import Profile from "@/models/Profile";
 import connectDB from "@/utils/api/connectDB";
-import { NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request) {
+export async function GET(req: NextRequest) {
   try {
+    const search = req.nextUrl.searchParams;
+    const category: string | null = search.get("category");
+
     await connectDB();
-    const profiles = await Profile.find().select("-userId");
-    return NextResponse.json({ profiles }, { status: 200 });
+    if (!category) {
+      const profiles = await Profile.find().select("-userId");
+      return NextResponse.json({ profiles }, { status: 200 });
+    } else {
+      const profiles = await Profile.find({ category }).select("-userId");
+
+      return NextResponse.json({ profiles }, { status: 200 });
+    }
   } catch (err) {
     console.log(err);
     return NextResponse.json(
