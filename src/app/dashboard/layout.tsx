85e3b56da0288cb6edbad10cd296a6d3b7baa3e6
Amir Basirati <amirbasirati13@gmail.com>
2023-11-21 15:26:09 +0330

    dashboard side bar is usable for admin now

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -2,11 +2,23 @@ import DashboardSidebar from "@/layouts/dashboard/DashboardSidebar";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 import { redirect } from "next/navigation";
+import { cookies } from "next/headers";
 
 const layout = async ({ children }: { children: React.ReactNode }) => {
   const session = await getServerSession(authOptions);
   if (!session) redirect("/signin");
-  return <DashboardSidebar>{children}</DashboardSidebar>;
+  const res = await fetch(`${process.env.NEXTAUTH_URL}/api/user`, {
+    headers: { Cookie: cookies().toString() },
+    cache: "no-store",
+  });
+  const { user } = await res.json();
+  console.log(user);
+  if (user.error) return <h3>{user.error}</h3>;
+  return (
+    <DashboardSidebar email={user.email} role={user.role}>
+      {children}
+    </DashboardSidebar>
+  );
 };
 
 export default layout;
--- src/components/layouts/dashboard/DashboardSidebar.tsx
+++ src/components/layouts/dashboard/DashboardSidebar.tsx
@@ -3,20 +3,21 @@ import LogoutButton from "@/modules/dashborad-layout/logout-button/LogoutButton"
 import { getServerSession } from "next-auth";
 import Link from "next/link";
 import { CgProfile } from "react-icons/cg";
+import { Role } from "src/types/enums";
 
-const DashboardSidebar = async ({
-  children,
-}: {
+interface Props {
+  email: string;
+  role: Role;
   children: React.ReactNode;
-}) => {
-  const session = await getServerSession(authOptions);
+}
+
+const DashboardSidebar: React.FC<Props> = async ({ children, email, role }) => {
   return (
     <div className="flex justify-between mt-20">
       <div className="flex flex-col items-center h-fit py-8 px-4 rounded-xl shadow-[0px_2px_10px] shadow-meBlue ml-10 w-56">
         <CgProfile className=" text-5xl text-meBlue" />
-        <p className=" text-gray-500 text-lg font-normal mt-5">
-          {session?.user?.email}
-        </p>
+        {role === Role.ADMIN ? <p>Admin</p> : null}
+        <p className=" text-gray-500 text-lg font-normal mt-5">{email}</p>
         <span className=" bg-slate-200 w-full h-[1px] mb-8"></span>
         <Link className=" my-1 font-normal w-full" href="/dashboard">
           حساب کاربری
@@ -30,6 +31,11 @@ const DashboardSidebar = async ({
         <Link className=" my-1 font-normal w-full" href="/dashboard/add">
           ثبت آگهی
         </Link>
+        {role === Role.ADMIN ? (
+          <Link className=" my-1 font-normal w-full" href={"/"}>
+            در انتظار تایید
+          </Link>
+        ) : null}
         <LogoutButton />
       </div>
       <div className="w-full">{children}</div>
